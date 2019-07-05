bc040540b8609e0b26cfee24058c3c7b58330d0a
Nikita Eliseenko <nikita.eliseenko@zensoft.io>
2019-07-05 16:33:54 +0300

    formula improvement

--- brew-test.rb
+++ brew-test.rb
@@ -1,17 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class BrewTest < Formula
-  desc ""
+  desc "Simple `hello Homebrew` application"
   homepage "https://github.com/nik1811e/brew-test"
-  url "https://github.com/nik1811e/brew-test/archive/v2.2.1.zip"
-  sha256 "16f9547c20ac9496f3bcc94d3e107fea9d4334bf6ac67de8eb2c5fdd11d8bd5b"
+  url "https://github.com/nik1811e/brew-test/archive/v4.0.1.tar.gz"
+  sha256 "8ab6cf1af10197246375c3dcf2a6b930609d790f81fb831d489a4576d6d54c3c"
 
+  depends_on :java
 
-  def install
-      inreplace "brew-test", "##PREFIX##", "#{prefix}"
-      prefix.install "brew-0.0.1.jar"
-      bin.install "srcclr"	
+    def install
+	inreplace "srcclr", "##PREFIX##", "#{prefix}"
+	inreplace "srcclr", "##VERSION##", "#{version}"
+	prefix.install "brew-#{version}.jar"
+	bin.install "srcclr"
   end
 
 end
